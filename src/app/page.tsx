b6833a2754ef30a02fdb08b606cb7881b1c4067e
Keith Broughton <keith.broughton@immutable.com>
2025-01-17 16:18:24 +1100

    Repro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,126 @@
-import Image from "next/image";
+"use client";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { checkout, config, passport } from "@imtbl/sdk";
+import { useSearchParams } from "next/navigation";
+export type CommerceContext = {
+  passportClient: passport.Passport;
+  widgetsFactory: checkout.IWidgetsFactory | undefined;
+  commerceWidget: checkout.Widget<typeof checkout.WidgetType.IMMUTABLE_COMMERCE> | undefined;
+};
+
+const commerceContext = createContext<CommerceContext | null>(null);
+const baseConfig = { environment: config.Environment.SANDBOX };
+
+function CommerceProvider({ children }: PropsWithChildren) {
+  const [passportClient] = useState<passport.Passport>(
+    () =>
+      new passport.Passport({
+        baseConfig,
+        clientId: process.env.NEXT_PUBLIC_CLIENT_ID!,
+        redirectUri: "http://localhost:3000",
+      })
+  );
+
+  const [widgetsFactory, setWidgetsFactory] =
+    useState<checkout.IWidgetsFactory>();
+
+  const [commerceWidget, setCommerceWidget] = useState<checkout.Widget<typeof checkout.WidgetType.IMMUTABLE_COMMERCE>>();
+
+  useEffect(() => {
+    (async () => {
+      console.log({ ui: await passportClient.getUserInfo() });
+      const checkoutSDK = new checkout.Checkout({
+        passport: passportClient,
+        baseConfig,
+      });
+      const widgetsFactory = await checkoutSDK.widgets({
+        config: {
+          theme: checkout.WidgetTheme.DARK,
+        },
+      });
+      setWidgetsFactory(widgetsFactory);
+      const commerceWidget = widgetsFactory.create(checkout.WidgetType.IMMUTABLE_COMMERCE, { });
+      setCommerceWidget(commerceWidget);
+    })();
+  }, []);
+
+  return (
+    <commerceContext.Provider value={{ passportClient, widgetsFactory, commerceWidget }}>
+      {children}
+    </commerceContext.Provider>
+  );
+}
+
+const useCommerce = () => {
+  const context = useContext(commerceContext);
+  if (!context) {
+    throw new Error("useCommerce must be used within a CommerceProvider");
+  }
+  return context;
+};
+
+export const CommerceWidget = ({
+  params,
+}: {
+  params: checkout.CommerceWidgetParams;
+}) => {
+  const { commerceWidget } = useCommerce();
+
+  useEffect(() => {
+    console.log(params.flow)
+    console.log({ commerceWidget, params });
+    if (!commerceWidget) return;
+    commerceWidget.mount("no-thanks", params);
+    return () => {
+      console.log("unmounting...");
+      commerceWidget.unmount();
+    };
+  }, [params, commerceWidget]);
+
+  return <div id="no-thanks" />;
+};
+
+function Thang() {
+  const { passportClient } = useCommerce();
+  const queryParams = useSearchParams();
+  const [showWidget, setShowWidget] = useState(false);
+
+  useEffect(() => {
+    if (!queryParams.has("code")) return;
+    passportClient.loginCallback();
+  }, [passportClient, queryParams]);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setShowWidget(true);
+        }}
+      >
+        Show
+      </button>
+      {showWidget ? (
+        <CommerceWidget params={{ flow: checkout.CommerceFlowType.SWAP, amount: "1" }} />
+      ) : null}
+    </>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <CommerceProvider>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <Thang />
+        </main>
+      </div>
+    </CommerceProvider>
   );
 }
